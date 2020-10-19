947de82a57118f790d43bdf35481e6e2e3504138
Rasmus Krämer <rasmus@iMac.fritz.box>
2020-10-19 18:01:11 +0200

    Icons are back

--- src/preload.js
+++ src/preload.js
@@ -4,7 +4,9 @@ document.addEventListener('websocketMessage|type:fcm-message', (event) => {
   
     if(data.link && window.location.href == data.link) return
 
-    const notificationObject = new Notification(notification.title, notification)
+    console.log(notification, data);
+
+    const notificationObject = new Notification(notification.title, data)
   
     notificationObject.addEventListener("click", () => {
       if(data.link) window.location = data.link
