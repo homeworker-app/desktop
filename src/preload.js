7f45c76a622f236716b3292f3f5424d75c01c00d
xolf <xolf@users.noreply.github.com>
2021-05-25 03:19:17 +0200

    Removed handling for push messages via websocket

--- src/preload.js
+++ src/preload.js
@@ -2,21 +2,6 @@ const { ipcRenderer } = require("electron")
 const os = require("os")
 const { getDisplayMedia } = require("./util/screenPicker")
 
-// Got notification from websocket
-document.addEventListener('websocketMessage|type:fcm-message', (event) => {
-  if(Notification.permission === "granted") {
-    const { notification, data } = event.message.payload
-  
-    if(data.link && window.location.href === data.link) return
-
-    const notificationObject = new Notification(notification.title, data)
-    notificationObject.addEventListener("click", () => {
-      if(data.link) window.location = data.link
-      notificationObject.close()
-    })
-  }
-})
-
 window.addEventListener("load", () => {
   window.desktop = {
     call: true,
